9f1ead4afb07918459ea216320cc6c5d229aefb9
Bartosz Wojdałowicz <bartoszwojdalowicz@gmail.com>
2024-02-08 15:18:12 +0100

    OP-232 - configure plugin skeleton for sulu plugin

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin\DependencyInjection;
+namespace BitBag\SyliusSuluPlugin\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -14,7 +14,7 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('acme_sylius_example_plugin');
+        $treeBuilder = new TreeBuilder('bitbag_sylius_sulu_plugin');
         $rootNode = $treeBuilder->getRootNode();
 
         return $treeBuilder;
