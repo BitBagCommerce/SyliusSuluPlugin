53262d109082b22a96034d4471bd912e29f00763
maciej.dejewski@bitbag.pl <maciej.dejewski@bitbag.pl>
2024-03-21 16:17:17 +0100

    OP-249 - add assets compilation to Test App

---
+++ tests/Application/assets/shop/img/index.js
@@ -0,0 +1,4 @@
+import './a-fashionable-young-man-in-suit-walking-down-the-stiars.jpg';
+import './fashion-magazines-on-table.jpg';
+import './laughing-woman-in-yellowish-dress-on-stairs.jpg';
+import './trench-coat-in-shopping-window.jpg';
