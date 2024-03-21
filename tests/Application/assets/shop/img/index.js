1ecec0a0306b05849422e10249d0e909d1799a19
maciej.dejewski@bitbag.pl <maciej.dejewski@bitbag.pl>
2024-03-21 16:17:34 +0100

    OP-249 - style Sulu templates in test App

--- tests/Application/assets/shop/img/index.js
+++ tests/Application/assets/shop/img/index.js
@@ -1,4 +1,4 @@
-import './a-fashionable-young-man-in-suit-walking-down-the-stiars.jpg';
-import './fashion-magazines-on-table.jpg';
-import './laughing-woman-in-yellowish-dress-on-stairs.jpg';
-import './trench-coat-in-shopping-window.jpg';
+import './a-fashionable-young-man-in-suit-walking-down-the-stiars.webp';
+import './fashion-magazines-on-table.webp';
+import './laughing-woman-in-yellowish-dress-on-stairs.webp';
+import './trench-coat-in-shopping-window.webp';
